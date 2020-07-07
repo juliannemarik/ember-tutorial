1233edc7253077662e41e8459f9316310d0fed39
juliannemarik <julianne.marik@gmail.com>
2020-07-07 08:43:39 -0500

    feat(chapter 4): routing

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+class Band {
+    @tracked name;
+    constructor(name) {
+        this.name = name;
+    }
+}
 
 export default class BandsRoute extends Route {
+    model() {
+        return [
+            new Band('Led Zeppelin'),
+            new Band('Pearl Jam'),
+            new Band('Foo Fighters')
+        ];
+    }
 }
