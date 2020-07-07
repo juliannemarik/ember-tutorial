fa3daa7f7a2d6fcf78b82889c07dc8b787334f3a
juliannemarik <julianne.marik@gmail.com>
2020-07-07 07:38:59 -0500

    feat(chapter 3): templates & data bindings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          { module: require('tailwindcss') }
+        ]
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
